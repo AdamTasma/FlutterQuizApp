660303aa7c38aa5628e0a34d508876d29393d18c
atasma <atasma@magicwrighter.com>
2019-11-27 14:57:34 -0500

    2-51 adding 'score' map to answers, and passsing it to quiz.dart in the form of an anonymous function

--- lib/main.dart
+++ lib/main.dart
@@ -19,29 +19,43 @@ class _MyAppState extends State<MyApp> {
     {
       // could also use Map(). its a key/value pair. "{}" is the shortcut way of writing Map()
       'questionText': 'What\'s your favorite color?',
-      'answers': ['blue', 'black', 'red', 'green'],
+      'answers': [
+        {'text': 'blue', 'score': '1'},
+        {'text': 'black', 'score': '2'},
+        {'text': 'red', 'score': '3'},
+        {'text': 'translucent', 'score': '4'},
+      ],
     },
     {
       'questionText': "What\'s your favorite animal?",
-      'answers': ['tiger', 'lion', 'bears', 'dogs'],
+      'answers': [
+        {'text': 'zebra', 'score': '1'},
+        {'text': 'flying squirrel', 'score': '2'},
+        {'text': 'lioness', 'score': '3'},
+        {'text': 'Butcher when not using his keyboard shortcuts', 'score': '4'},
+      ],
     },
     {
-      'questionText': 'Who\'s your favorite wife?',
-      'answers': ['Aubrey', 'Aubrey', 'Aubrey', 'Aubrey'],
+      'questionText': 'Who\'s the best wife?',
+      'answers': [
+        {'text': 'Aubrey', 'score': '1'},
+        {'text': 'Aubrey', 'score': '2'},
+        {'text': 'Aubrey', 'score': '3'},
+        {'text': 'Aubrey', 'score': '4'},
+      ],
     }
   ];
 
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore = _totalScore + score;
+
     setState(() {
       _questionIndex++;
     });
     print(_questionIndex);
-
-//    if (_questionIndex < questions.length) {
-//      print('we have more questions');
-//    }
   }
 
   @override
